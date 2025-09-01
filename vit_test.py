da24fc5ea5558843ebac75a54deaf4a6dcc11273
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2025-09-02 01:41:46 +0530

    Model Training of ViT and CNN on CIFAR10 Dataset

--- vit_test.py
+++ vit_test.py
@@ -4,43 +4,62 @@ from src.train import PyTorchTrainer
 from src.data_loaders import get_dataloaders
 
 if __name__ == "__main__":
-    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # Choose dataset: "cifar10" or "mnist"
-    print("Loading the dataset......")
+    print("Loading the CIFAR Dataset......")
     dataset = "cifar10"  
     train_loader, val_loader, test_loader, num_classes = get_dataloaders(
         dataset, batch_size=64, img_size=224, val_split=0.1
     )
 
-    # Model
-    print("Loading the model......")
-    pretrained_cls = CustomViTClassifier(
-        num_classes=num_classes, model_name="vit_b_16", img_size=224, device=device
-    )
+    print("Loading the CNN Model......")
     cnn_cls = ResNetMLP(
         num_classes=num_classes
-)
+    )
+    # Model
+    print("Loading the ViT Model......")
+    vit_cls = CustomViTClassifier(
+        num_classes=num_classes, model_name="vit_b_16", img_size=224
+    )
+
 
     # Trainer
-    print("Loading the trainer......")
-    trainer = PyTorchTrainer(
-        model=pretrained_cls,
+    print("Loading the CNN Trainer......")
+    cnn_trainer = PyTorchTrainer(
+        model=cnn_cls,
         train_loader=train_loader,
         val_loader=val_loader,
-        device=device,
+        mixed_precision=True,
+    )
+    print("Loading the ViT Trainer......")
+    vit_trainer = PyTorchTrainer(
+        model=vit_cls,
+        train_loader=train_loader,
+        val_loader=val_loader,
         mixed_precision=True,
     )
 
-    print("Training the model......")
-    # Train for a few epochs
-    history = trainer.fit(epochs=1)
 
-    print("Evaluating the model.....")
+    print("Training the CNN Model......")
+    # Train for a few epochs
+    history = cnn_trainer.fit(epochs=1)
+
+    print("Evaluating the CNN Model.....")
     # (Optional) Evaluate on test set
-    test_loss, test_acc = trainer.evaluate(test_loader)
+    test_loss, test_acc = cnn_trainer.evaluate(test_loader)
     print(f"Final Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
 
-    print("Saving the model...")
-    torch.save(pretrained_cls.state_dict(), "model.pth")
+    print("Saving the CNN Model...")
+    torch.save(cnn_cls.state_dict(), "cnn_model.pth")
+
+    print("Training the ViT Model......")
+    # Train for a few epochs
+    history = vit_trainer.fit(epochs=1)
+
+    print("Evaluating the ViT Model.....")
+    # (Optional) Evaluate on test set
+    test_loss, test_acc = vit_trainer.evaluate(test_loader)
+    print(f"Final Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
+
+    print("Saving the ViT Model...")
+    torch.save(vit_cls.state_dict(), "vit_model.pth")
 
